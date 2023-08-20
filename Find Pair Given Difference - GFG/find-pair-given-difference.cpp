5ce0f04edd7e1f282b8a2cb273377a87d8bef52f
SNEHA CHAUHAN <94298791+i-am-SnehaChauhan@users.noreply.github.com>
2023-08-20 22:28:08 +0530

    Added solution - LeetHub

--- Find Pair Given Difference - GFG/find-pair-given-difference.cpp
+++ Find Pair Given Difference - GFG/find-pair-given-difference.cpp
@@ -28,15 +28,20 @@ int main()
 // } Driver Code Ends
 
 
+#include <unordered_set>
+
 bool findPair(int arr[], int size, int n){
-    sort(arr,arr+size);
-    for(int i=0; i<size; i++){
-        for(int j=i+1; j<size; j++){
-            if((arr[j]-arr[i])==n){
-                return true;
-            }
-        }
-    }
-    return false;
+    std::unordered_set<int> numSet; // Create a hash set to store numbers
     
+    for(int i = 0; i < size; i++){
+        // Check if (arr[i] - n) or (arr[i] + n) exists in the set
+        if(numSet.count(arr[i] - n) || numSet.count(arr[i] + n)){
+            return true;
+        }
+        
+        // Insert current element into the set
+        numSet.insert(arr[i]);
+    }
+    
+    return false;
 }
