daa21cc8b3786da4a5323fdfcdd9e6f4e593827f
SNEHA CHAUHAN <94298791+i-am-SnehaChauhan@users.noreply.github.com>
2023-09-14 20:42:32 +0530

    Added solution - LeetHub

---
+++ Elements in the Range - GFG/elements-in-the-range.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+	{
+		unordered_set<int> set;
+		for(int i=0; i<n; i++){
+		    set.insert(arr[i]);
+		}
+		for(int i=A; i<=B; i++){
+		    if(!set.count(i)){
+		        return false;
+		    }
+		}
+		return true;
+	}
+
+
+		 
+
+};
+	
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,A,B;
+		cin>>n>>A>>B;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+
+        
+
+        Solution ob;
+        if (ob.check_elements(a, n, A, B))
+			cout << "Yes";
+		else
+			cout << "No";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends
